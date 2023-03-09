84b1993199ec9b0113759aa31fb8a30613e7cd5d
Astrilate <122201901+Astrilate@users.noreply.github.com>
2023-03-09 21:39:42 +0800

    Add files via upload

---
+++ c++.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+using namespace std;
+
+
+class Ticket{
+	public:
+		Ticket(int k);
+		void function(int a);
+	private:
+		float x;
+};
+
+
+Ticket::Ticket(int k)
+{
+	x = k;
+}
+
+
+void Ticket::function(int j)
+{
+	int z;
+	cout << x << "," << j << endl;
+	cin >> z;
+	cout << z << endl;  
+}
+
+
+
+int main(){
+	Ticket dx(10); 
+	dx.function(20);
+// 	cout << dx.x << endl;
+	return 0;
+}
